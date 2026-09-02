30fec517d473b3d3ab94f9e59aa13f9ec4dab9ec
EEProsper <efosa.eriamiatoe@pau.edu.ng>
2026-09-02 18:59:50 +0100

    practice 2

---
+++ week-2/practice_2.rs
@@ -0,0 +1,7 @@
+fn main() {
+	println!(); // prints just a
+	println!("hello"); // prints
+	println!("this {} is confusing", "thing" ); //prints format some arguments
+	println!("My name is {},and this computer science thing is stressful mehnn","Efosa" );
+
+}

f35679b98598040593a3e887102f022fafb74ac7
EEProsper <efosa.eriamiatoe@pau.edu.ng>
2026-09-19 14:35:27 +0100

    complete week _4

--- week_4/project_1/src/main.rs
+++ week_4/project_1/src/main.rs
@@ -5,19 +5,19 @@ fn main() {
     println!("What is the coefficient of x square in your equation?");
     let mut input1 = String::new();
     io::stdin().read_line(&mut input1).expect("Not valid float");
-    let a = input1.trim();
+    let a:f32 = input1.trim().parse().expect("Not valid number");
 
     println!("What is the coefficient of x in your equation?");
     let mut input2 = String::new();
     io::stdin().read_line(&mut input2).expect("Not valid float");
-    let b = input2.trim();
+    let b:f32 = input2.trim().parse().expect("Not valid number");
 
     println!("What is the value of the number that does not have x in your eqaution?");
     let mut input3 = String::new();
     io::stdin().read_line(&mut input3).expect("Not valid float");
-    let c = input3.trim();
+    let c:f32 = input3.trim().parse().expect("Not valid number");
 
-    let mut d:f32 = (b*b) - (4.0 * a * c);
+    let d:f32 = (b*b) - (4.0 * a * c);
     if d > 0.0 {let r1: f32 = (-b + ((b * b) - (4.0 * a * c)).sqrt()) / (2.0 * a);
         let r2: f32 = (-b - ((b * b) - (4.0 * a * c)).sqrt()) / (2.0 * a);
         println!("Your equation has two distinct roots which are {} and {}",r1,r2 );}
---
+++ week_4/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Documents\e..eriamiatoeCOS101\week_4\project_1\target\debug\project_1.exe: C:\Users\USER\Documents\e..eriamiatoeCOS101\week_4\project_1\src\main.rs
